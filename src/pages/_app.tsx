a546acf7350ecb89c8bc84ddffc72639f20de20f
Janet Cash <janet.wy.adams@gmail.com>
2025-09-11 09:02:57 -0400

    added middlware ts file and updated app tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,41 @@
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
+import { AppProps } from 'next/app';
+import { useState } from 'react';
+import Navbar from '../components/Navbar';
+import '../styles/globals.css';
+
+function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Navbar />
+      <main className="max-w-4xl mx-auto p-4">
+        <Component {...pageProps} />
+      </main>
+    </SessionContextProvider>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // pages/_app.tsx
 /**
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
@@ -31,7 +69,7 @@ function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
 export default App;
 */
 
-
+/*
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 import { AppProps } from 'next/app';
@@ -63,3 +101,4 @@ function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
 }
 
 export default App;
+*/
