acd16ec1f48ec13579053a99854ba73d00606b0c
Janet Cash <janet.wy.adams@gmail.com>
2025-10-03 08:06:50 -0400

    fixing mobile view issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,9 +17,18 @@ const SearchIcon = () => (
 );
 
 // mobile menu hamburger icon
+//const MenuIcon = () => (
+  //  <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    //    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+    //</svg>
+//);
+
+// mobile menu hamburger icon - three lines
 const MenuIcon = () => (
-    <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <rect x="4" y="6" width="16" height="2" />
+        <rect x="4" y="11" width="16" height="2" />
+        <rect x="4" y="16" width="16" height="2" />
     </svg>
 );
 
