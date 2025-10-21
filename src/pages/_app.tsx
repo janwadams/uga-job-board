3ae8b12608e7c911607d813e746d51e95f3be2a3
Janet Cash <janet.wy.adams@gmail.com>
2025-10-21 15:09:52 -0400

    footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,9 @@ import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 import { AppProps } from 'next/app';
 import { useState } from 'react';
-import Navbar from 'components/Navbar';
-import Footer from 'components/Footer'; // added for footer
-import 'styles/globals.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer'; // added for footer
+import '../styles/globals.css';
 import '../styles/mobile-fixes.css';
 
 function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
