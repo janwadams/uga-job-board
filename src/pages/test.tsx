9408c42f0c2502aab26e8428fcf4835d78419293
Janet Cash <janet.wy.adams@gmail.com>
2025-10-21 15:41:20 -0400

    footer

---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+// src/pages/test.tsx
+import Footer from 'components/Footer';
+
+export default function Test() {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <Footer />
+    </div>
+  );
+}
