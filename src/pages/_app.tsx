546ac14b3573535595792c536ac9a2672d514823
Janet Cash <janet.wy.adams@gmail.com>
2025-10-22 11:58:56 -0400

    footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,10 @@ import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 import { AppProps } from 'next/app';
 import { useState } from 'react';
 
-
+import Footer from 'components/Footer'; 
 import Navbar from 'components/Navbar';
 
-import Footer from '/src/components/Footer'; 
+
 import 'styles/globals.css';
 import '../styles/mobile-fixes.css';
 
