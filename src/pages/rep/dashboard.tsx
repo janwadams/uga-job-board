ba8b7b95e9b70481a441c398ecf35a04b0956b07
Janet Cash <janet.wy.adams@gmail.com>
2025-11-21 09:06:36 -0500

    rep db

--- src/pages/rep/dashboard.tsx
+++ src/pages/rep/dashboard.tsx
@@ -465,7 +465,7 @@ export default function RepDashboard() {
                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
               } whitespace-nowrap py-4 px-1 border-b-2 font-medium text-lg`}
             >
-              Current Jobs ({jobs.length})
+              Jobs ({jobs.length})
             </button>
             <button 
               onClick={() => setActiveTab('rejected')} 
