0f8880e552e28af8a7cf8c5b3aace25af2b6ebf2
Janet Cash <janet.wy.adams@gmail.com>
2025-11-21 09:02:31 -0500

    fac db

--- src/pages/faculty/dashboard.tsx
+++ src/pages/faculty/dashboard.tsx
@@ -394,7 +394,7 @@ export default function FacultyDashboard() {
                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
               } whitespace-nowrap py-4 px-1 border-b-2 font-medium text-lg`}
             >
-              Active Jobs ({jobs.length})
+              Jobs ({jobs.length})
             </button>
             <button 
               onClick={() => setActiveTab('archived')} 
